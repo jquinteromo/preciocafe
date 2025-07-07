987e91c9465c99293e825f6edea9275df6a2b470
jquinteromo <qjaider1111@gmail.com>
2025-07-06 19:41:07 -0500

    enable cors origin

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 import os
 from flask import Flask, jsonify
+from flask_cors import CORS
 from scraper import obtener_precio_cafe
 
 app = Flask(__name__)
+CORS(app)
+
 
 @app.route("/", methods=["GET"])
 def home():
